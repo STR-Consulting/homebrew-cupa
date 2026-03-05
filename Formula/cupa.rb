4c72c70a3147fc7fdcccdb01c8ff13fd8b4abbee
Jason Abbott <Jason-Abbott@users.noreply.github.com>
2026-03-05 13:07:18 -0700

    fix: correct asset URL to include version, add sha256

--- Formula/cupa.rb
+++ Formula/cupa.rb
@@ -5,8 +5,8 @@ class Cupa < Formula
   license "MIT"
 
   on_arm do
-    url "https://github.com/STR-Consulting/cupa/releases/download/v0.4.0/cupa_darwin_arm64.tar.gz"
-    sha256 ""
+    url "https://github.com/STR-Consulting/cupa/releases/download/v#{version}/cupa_#{version}_darwin_arm64.tar.gz"
+    sha256 "d7f63d6a020a4825f217746b4a4d0d5cc3ff0712caca0265fafaf4334e09ed6b"
   end
 
   def install
