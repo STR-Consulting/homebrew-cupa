1f83bece272098374617f1f6a16cd7318f9c4a6d
Jason Abbott <Jason-Abbott@users.noreply.github.com>
2026-03-05 13:33:27 -0700

    fix formula: correct archive name and add sha256

--- Formula/cupa.rb
+++ Formula/cupa.rb
@@ -5,11 +5,12 @@ class Cupa < Formula
   license "MIT"
 
   on_arm do
-    url "https://github.com/STR-Consulting/cupa/releases/download/v0.6.0/cupa_darwin_arm64.tar.gz"
-    sha256 ""
+    url "https://github.com/STR-Consulting/cupa/releases/download/v0.6.0/cupa_0.6.0_darwin_arm64.tar.gz"
+    sha256 "c7394b81084b07012f8c03486dea5b8e0c3de65056780901fbe10b971b0f61a0"
   end
 
   def install
     bin.install "cupa" => "cupa"
   end
 end
+
